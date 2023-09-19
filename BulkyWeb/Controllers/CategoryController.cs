ac1257d035190f1ec9325c848fb8f4064982ff8a
Olivia Wyand <owyand22@gmail.com>
2023-09-19 10:52:30 -0400

    update UI for creating categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -32,6 +32,57 @@ namespace BulkyWeb.Controllers
             {
                 ModelState.AddModelError("name", "The Display Order and Name cannot be the same");
             }
+            //custom validation - name cannot be test
+            if (obj.Name == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category catFromDb = _db.Categories.Find(id);
+            Category catFromDb1 = _db.Categories.FirstOrDefaut(id);
+            Category catFromDb2 = _db.Categories.Find(id);
+
+            if (catFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(catFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            //custom validation - name and display order cannot be same value
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order and Name cannot be the same");
+            }
+            //custom validation - name cannot be test
+            if (obj.Name == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
