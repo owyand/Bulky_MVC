1120ae0f775ef021c5a5116676cf28c13b4be04a
Olivia Wyand <owyand22@gmail.com>
2023-10-01 11:52:26 -0400

    completed CRUD operations with creating, updating and deleting Category types in the category controller, use of tostr

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -42,6 +42,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
 
                 return RedirectToAction("Index");
             }
@@ -57,9 +58,9 @@ namespace BulkyWeb.Controllers
             }
 
             //works on primary key (which ID is)
-            Category catFromDb = _db.Categories.Find(id);
+            Category? catFromDb = _db.Categories.Find(id);
             //works on any property -- preferred
-            Category catFromDb1 = _db.Categories.FirstOrDefaut(u => u.Id == id);
+            Category catFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
             //also works for any property
             Category catFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
 
@@ -74,27 +75,59 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-
-            //custom validation - name and display order cannot be same value
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The Display Order and Name cannot be the same");
-            }
-            //custom validation - name cannot be test
-            if (obj.Name == "test")
-            {
-                ModelState.AddModelError("", "Test is an invalid value");
-            }
-
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+
 
                 return RedirectToAction("Index");
             }
 
             return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //works on primary key (which ID is)
+            Category? catFromDb = _db.Categories.Find(id);
+            //works on any property -- preferred
+            Category catFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //also works for any property
+            Category catFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (catFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(catFromDb);
+        }
+
+        //customizes the action URL
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+
+            Category obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
+
+            return RedirectToAction("Index");
         }
     }
 }
