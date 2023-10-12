b033a1ba05495041d4150b518a5f7914f22d61d3
Olivia Wyand <owyand22@gmail.com>
2023-10-12 16:42:59 -0400

    large commit

---
+++ BulkyWebRazor/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,28 @@
+using BulkyWebRazor.Data;
+using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToPage("index");
+        }
+    }
+}
---
+++ BulkyWebRazor/Program.cs
@@ -0,0 +1,29 @@
+using BulkyWebRazor.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=DESKTOP-UONUHON;Database=Bulky_Razor;Trusted_Connection=True;TrustServerCertificate=True"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
